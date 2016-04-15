9fa3ac5b4c1a16814b697bab8cc69e9faec18774
Alexandr Gribkov <alexandr.gribkov@yopeso.com>
2016-04-15 12:02:58 +0300

    add schema to hw7

---
+++ homework07/controller/user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const userModel = require('../model/user');
+
+
+let mary = new userModel({
+  email: 'mary@mail.com'
+});
+
+// no error handling here (bad)
+userModel.remove({}, function(err) {
+
+  mary.save(function(err, result) {
+    console.log(result);
+
+    userModel.findOne({
+      email: 'mary@mail.com'
+    }, function(err, user) {
+      console.log(user);
+
+      mongoose.disconnect();
+    });
+
+  });
+
+});
